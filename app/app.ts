94d9a9f3b4c8993c5c5c384040931bcb6b09b081
Michal Bryxí <michal.bryxi@gmail.com>
2024-08-13 22:15:29 +0200

    feat: Add glint

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'winds-mobi-client-web/config/environment';
+import '@glint/environment-ember-loose'; // Because: https://typed-ember.gitbook.io/glint/environments/ember/installation
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
