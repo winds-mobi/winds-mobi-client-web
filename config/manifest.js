d6756b9a91107b213b1d5b5c3913847907c386e2
Michal Bryxí <michal.bryxi@gmail.com>
2024-08-27 15:12:40 +0200

    feat: PWA icons

--- config/manifest.js
+++ config/manifest.js
@@ -14,7 +14,17 @@ module.exports = function (/* environment, appConfig */) {
     display: 'standalone',
     background_color: '#fff',
     theme_color: '#fff',
-    icons: [],
+    icons: [
+      {
+        src: '/assets/icons/appicon-32.png',
+        sizes: '32x32',
+        targets: ['favicon'],
+      },
+      ...[192, 280, 512].map((size) => ({
+        src: `/assets/icons/appicon-${size}.png`,
+        sizes: `${size}x${size}`,
+      })),
+    ],
     ms: {
       tileColor: '#fff',
     },
