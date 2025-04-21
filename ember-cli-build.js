a79dcde9f6901e66a179ceed95c9b394ac149922
Michal Bryxí <michal.bryxi@gmail.com>
2025-04-21 14:10:08 +0200

    fix: Manual patches for ember-leaflet

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,30 @@ const { compatBuild } = require('@embroider/compat');
 module.exports = async function (defaults) {
   const { setConfig } = await import('@warp-drive/build-config');
   const { buildOnce } = await import('@embroider/vite');
-  let app = new EmberApp(defaults, {});
+
+  let options = {
+    'ember-cli-image-transformer': {
+      images: [
+        {
+          inputFilename: 'public/images/windmobile.png',
+          outputFileName: 'appicon-',
+          convertTo: 'png',
+          destination: 'assets/icons/',
+          sizes: [32, 192, 280, 512],
+        },
+      ],
+    },
+  };
+
+  let app = new EmberApp(defaults, {
+    'ember-cli-babel': { enableTypeScriptTransform: true },
+    babel: {
+      plugins: [
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+      ],
+    },
+    ...options,
+  });
 
   setConfig(app, __dirname, {
     // WarpDrive/EmberData settings go here (if any)
