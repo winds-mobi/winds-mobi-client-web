ceaa7c1c0d120593d7f43cbb0f718b8d36428706
Michal Bryxí <michal.bryxi@gmail.com>
2025-04-21 13:44:40 +0200

    fix: buildCOnfig for warp-drive

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,8 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const { compatBuild } = require('@embroider/compat');
 
 module.exports = async function (defaults) {
+  const { setConfig } = await import('@warp-drive/build-config');
   const { buildOnce } = await import('@embroider/vite');
   let app = new EmberApp(defaults, {});
+
+  setConfig(app, __dirname, {
+    // WarpDrive/EmberData settings go here (if any)
+  });
 
   return compatBuild(app, buildOnce);
 };
