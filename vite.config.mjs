e3d05e7990c661c94a24c4dbcfaf5b559c5ac676
Michal Bryxí <michal.bryxi@gmail.com>
2025-10-02 14:18:56 +0200

    feat: vite-pwa

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,8 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  server: {
+    allowedHosts: ['ui.winds-mobi-client-web.orb.local'],
+  },
   plugins: [
     classicEmberSupport(),
     ember(), // extra plugins here
@@ -12,6 +16,46 @@ export default defineConfig({
       extensions,
     }),
     tailwindcss(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'icons/pwa-*/**/*.png'], // include generated icons
+      manifest: {
+        name: 'winds.mobi',
+        short_name: 'winds.mobi',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#4E9805',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
   ],
   optimizeDeps: {
     exclude: ['ember-leaflet', 'ember-page-title', 'object-inspect'],
